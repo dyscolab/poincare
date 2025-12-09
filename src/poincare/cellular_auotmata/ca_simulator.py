0950fe87f296cf8e6f37278386f1f12a2cac9220
Oliverio Starosta <oliveriostarosta@gmail.com>
2025-12-09 10:14:01 -0300

    Added general automata class, diffusion test for square automata

--- src/poincare/cellular_auotmata/ca_simulator.py
+++ src/poincare/cellular_auotmata/ca_simulator.py
@@ -8,82 +8,34 @@ import numpy as np
 from numpy import ndarray
 import pandas as pd
 
-from symbolite import Symbol
+from symbolite import Symbol, real
 
-from .ca_types import Automata, SquareAutomata
+from .ca_types import Automata, SquareAutomata, CellType, ArbitraryInitial
 from ..types import System
 from ..compile import Backend, build_first_order_symbolic_ode, Array, MutableArray
-from ..simulator import Simulator, Problem
+from ..simulator import Simulator, Problem, Components
 from ..solvers import LSODA
 from ..types import Variable, Derivative, Parameter, Constant, Literal, Number, Initial
 
 
 @dataclass
 class CASimulator:
-    # model: Automata
-    model: SquareAutomata
+    model: Automata
     shape: tuple[int]
-    backend: Backend = "numpy"
     transform: Sequence[Symbol] | Mapping[Hashable, Symbol] | None = None
 
     def __post_init__(self):
-        self.cell_sim = Simulator(self.model.cell, backend=self.backend)
-        self.interact_sim = Simulator(
-            self.model.interaction_system, backend=self.backend
-        )
-        self.boundary_sim = Simulator(self.model.boundary_system, backend=self.backend)
-
-        self.symbolic_cell = build_first_order_symbolic_ode(self.model.cell)
-
         self.geometry = self.model.geometry(self.shape)
         self.func = self.create_func()
 
-    def create_default_initials(self, variable_index: int, values) -> Array:
-        y_0 = self.cell_sim.create_problem().y[variable_index]
-        return np.full(np.prod(self.shape), y_0)
-
-    def create_initials(self, values) -> Array:
-        vars = self.symbolic_cell.variables
-        intials = np.array(
-            [
-                np.ravel(
-                    values.get(
-                        vars[i],
-                        self.create_default_initials(variable_index=i, values=values),
-                    ).astype(float, copy=False)
-                )
-                for i in range(len(vars))
-            ]
-        )
-        return np.ravel(np.column_stack(intials))
-
-    def create_default_parameters(self, parameter_index: int, values) -> Array:
-        p_0 = self.cell_sim.create_problem().p[parameter_index]
-        return np.full(np.prod(self.shape), p_0)
-
-    def create_parameters(self, values) -> Array:
-        params = self.symbolic_cell.parameters
-        try:
-            intials = np.array(
-                [
-                    np.ravel(
-                        values.get(
-                            params[i], self.create_default_parameters(i, values=values)
-                        ).astype(float, copy=False)
-                    )
-                    for i in range(len(params))
-                ]
-            )
-            return np.ravel(np.column_stack(intials))
-        except ValueError:
-            return np.array([])
-
     def create_func(self):
         def func(t: float, y: Array, p: Array, dy: MutableArray):
-            for index in self.geometry.yield_locations():
-                var_num = len(self.symbolic_cell.variables)
-                param_num = len(self.symbolic_cell.parameters)
-                one_d_index = self.geometry.get(index)
+            for location, cell_category, index in self.geometry.yield_locations():
+                var_num = len(self.model.symbolic_cells[cell_category][index].variables)
+                param_num = len(
+                    self.model.symbolic_cells[cell_category][index].parameters
+                )
+                one_d_index = self.geometry.get(location)
                 one_d_slice = slice(var_num * one_d_index, var_num * (one_d_index + 1))
                 parameter_slice = slice(
                     param_num * one_d_index, param_num * (one_d_index + 1)
@@ -125,8 +77,16 @@ class CASimulator:
     def create_problem(
         self, values: Mapping[Variable | Parameter | Constant, ndarray], t_span
     ):
-        y = self.create_initials(values=values)
-        p = self.create_parameters(values=values)
+        y = self.geometry.create_initials(
+            symbolic_cells=self.model.symbolic_cells,
+            cell_sims=self.model.cell_sims,
+            values=values,
+        )
+        p = self.geometry.create_parameters(
+            symbolic_cells=self.model.symbolic_cells,
+            cell_sims=self.model.cell_sims,
+            values=values,
+        )
         return Problem(
             self.func,
             t_span,
@@ -140,14 +100,19 @@ class CASimulator:
         self,
         *,
         save_at: Sequence[Number],
-        values: Mapping[Variable | Parameter | Constant, ndarray] | None = None,
+        values: Mapping[
+            CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial]]
+        ]
+        | None = None,
         solver=LSODA(),
         format: Literal["dataframe", "array"] = "dataframe",
     ):
+        # TODO: fix creation of default values to fit format required by create_initials()
         values = values if values is not None else {}
         t_span = (save_at[0], save_at[-1])
         problem = self.create_problem(values=values, t_span=t_span)
         solution = solver(problem, save_at=np.asarray(save_at))
+        # TODO: fix output formatting
         output = solution.y.reshape(
             np.concatenate(
                 [[len(save_at)], self.shape, [len(self.symbolic_cell.variables)]]
@@ -222,3 +187,207 @@ def animate_array(frames, interval=100, **kwargs):
     ani = FuncAnimation(fig, update, frames=len(frames), interval=interval, blit=True)
 
     return ani
+
+
+# @dataclass
+# class SquareCASimulator:
+#     # model: Automata
+#     model: SquareAutomata
+#     shape: tuple[int]
+#     backend: Backend = "numpy"
+#     transform: Sequence[Symbol] | Mapping[Hashable, Symbol] | None = None
+
+#     def __post_init__(self):
+#         self.cell_sim = Simulator(self.model.cell, backend=self.backend)
+#         self.interact_sim = Simulator(
+#             self.model.interaction_system, backend=self.backend
+#         )
+#         self.boundary_sim = Simulator(self.model.boundary_system, backend=self.backend)
+
+#         self.symbolic_cell = build_first_order_symbolic_ode(self.model.cell)
+
+#         self.geometry = self.model.geometry(self.shape)
+#         self.func = self.create_func()
+
+#     def create_default_initials(self, variable_index: int, values) -> Array:
+#         y_0 = self.cell_sim.create_problem().y[variable_index]
+#         return np.full(np.prod(self.shape), y_0)
+
+#     def create_initials(self, values) -> Array:
+#         vars = self.symbolic_cell.variables
+#         intials = np.array(
+#             [
+#                 np.ravel(
+#                     values.get(
+#                         vars[i],
+#                         self.create_default_initials(variable_index=i, values=values),
+#                     ).astype(float, copy=False)
+#                 )
+#                 for i in range(len(vars))
+#             ]
+#         )
+#         return np.ravel(np.column_stack(intials))
+
+#     def create_default_parameters(self, parameter_index: int, values) -> Array:
+#         p_0 = self.cell_sim.create_problem().p[parameter_index]
+#         return np.full(np.prod(self.shape), p_0)
+
+#     def create_parameters(self, values) -> Array:
+#         params = self.symbolic_cell.parameters
+#         try:
+#             intials = np.array(
+#                 [
+#                     np.ravel(
+#                         values.get(
+#                             params[i], self.create_default_parameters(i, values=values)
+#                         ).astype(float, copy=False)
+#                     )
+#                     for i in range(len(params))
+#                 ]
+#             )
+#             return np.ravel(np.column_stack(intials))
+#         except ValueError:
+#             return np.array([])
+
+#     def create_func(self):
+#         def func(t: float, y: Array, p: Array, dy: MutableArray):
+#             for index in self.geometry.yield_locations():
+#                 var_num = len(self.symbolic_cell.variables)
+#                 param_num = len(self.symbolic_cell.parameters)
+#                 one_d_index = self.geometry.get(index)
+#                 one_d_slice = slice(var_num * one_d_index, var_num * (one_d_index + 1))
+#                 parameter_slice = slice(
+#                     param_num * one_d_index, param_num * (one_d_index + 1)
+#                 )
+#                 dy[one_d_slice] = self.cell_sim.compiled.func(
+#                     t, y[one_d_slice], p[parameter_slice], dy[one_d_slice]
+#                 )
+#                 neighbors = self.geometry.iter_neighbors(index)
+#                 for neighbor, interaction in neighbors:
+#                     if interaction == "cell_interaction":
+#                         neighbor_one_d_index = self.geometry.get(neighbor)
+#                         neighbor_one_d_slice = slice(
+#                             var_num * neighbor_one_d_index,
+#                             var_num * (neighbor_one_d_index + 1),
+#                         )
+#                         neighbor_parameter_slice = slice(
+#                             param_num * neighbor_one_d_index,
+#                             param_num * (neighbor_one_d_index + 1),
+#                         )
+#                         dy[one_d_slice] += self.interact_sim.compiled.func(
+#                             t,
+#                             np.concatenate((y[one_d_slice], y[neighbor_one_d_slice])),
+#                             np.concatenate(
+#                                 (p[parameter_slice], p[neighbor_parameter_slice])
+#                             ),
+#                             np.zeros(2 * var_num, dtype=float),
+#                         )[:var_num]
+#                     if interaction == "boundary":
+#                         dy[one_d_slice] += self.boundary_sim.compiled.func(
+#                             t,
+#                             y[one_d_slice],
+#                             p[parameter_slice],
+#                             np.zeros(var_num, dtype=float),
+#                         )
+#             return dy
+
+#         return func
+
+#     def create_problem(
+#         self, values: Mapping[Variable | Parameter | Constant, ndarray], t_span
+#     ):
+#         y = self.create_initials(values=values)
+#         p = self.create_parameters(values=values)
+#         return Problem(
+#             self.func,
+#             t_span,
+#             y,
+#             p,
+#             transform=lambda t, y, p, dy: y,
+#             scale=np.ones_like(y),
+#         )
+
+#     def solve(
+#         self,
+#         *,
+#         save_at: Sequence[Number],
+#         values: Mapping[Variable | Parameter | Constant, ndarray] | None = None,
+#         solver=LSODA(),
+#         format: Literal["dataframe", "array"] = "dataframe",
+#     ):
+#         values = values if values is not None else {}
+#         t_span = (save_at[0], save_at[-1])
+#         problem = self.create_problem(values=values, t_span=t_span)
+#         solution = solver(problem, save_at=np.asarray(save_at))
+#         if len(self.symbolic_cell.variables) > 1:
+#             output = solution.y.reshape(
+#                 np.concatenate(
+#                     [[len(save_at)], self.shape, [len(self.symbolic_cell.variables)]]
+#                 )
+#             )
+#         else:
+#             output = solution.y.reshape(np.concatenate([[len(save_at)], self.shape]))
+#         if format == "dataframe":
+#             cols = pd.MultiIndex.from_product(
+#                 [range(s) for s in self.shape]
+#                 + [[str(var) for var in self.symbolic_cell.variables]]
+#             )
+#             return pd.DataFrame(
+#                 output.reshape(
+#                     len(save_at),
+#                     np.prod(self.shape) * len(self.symbolic_cell.variables),
+#                 ),
+#                 columns=cols,
+#             )
+#         if format == "array":
+#             return output
+
+#     def solve_and_animate(
+#         self,
+#         *,
+#         save_at: Sequence[Number],
+#         values: Mapping[Variable | Parameter | Constant, ndarray] | None = None,
+#         solver=LSODA(),
+#         variable: Variable,
+#         timescale: Number = 1,
+#         save_to: str | None = None,
+#         **kwargs,
+#     ):
+#         if len(self.shape) != 2:
+#             raise ValueError("only 2d automata can be animated")
+#         full_result = self.solve(
+#             save_at=save_at, values=values, solver=solver, format="array"
+#         )
+#         var_index = self.symbolic_cell.variables.index(variable)
+#         var_num = len(self.symbolic_cell.variables)
+#         result = full_result[..., var_index::var_num].reshape(
+#             np.concatenate([[len(save_at)], self.shape])
+#         )
+#         interval = (save_at[1] - save_at[0]) * timescale * 1000
+#         animation = animate_array(result, interval, **kwargs)
+#         if isinstance(save_to, str):
+#             animation.save(save_to, writer="pillow")
+#         else:
+#             return animation
+
+
+# def animate_array(frames, interval=100, **kwargs):
+#     """
+#     frames: numpy array of shape (n_frames, H, W)
+#     interval: delay between frames in ms
+#     """
+#     from matplotlib.animation import FuncAnimation
+#     import matplotlib.pyplot as plt
+
+#     fig, ax = plt.subplots()
+#     im = ax.imshow(frames[0], animated=True, **kwargs)
+#     ax.axis("off")
+#     ax.set_frame_on(True)
+
+#     def update(frame_idx):
+#         im.set_array(frames[frame_idx])
+#         return [im]
+
+#     ani = FuncAnimation(fig, update, frames=len(frames), interval=interval, blit=True)
+
+#     return ani
--- src/poincare/cellular_auotmata/ca_types.py
+++ src/poincare/cellular_auotmata/ca_types.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 from itertools import product
 
 from ..types import System
-from numpy import ndarray
 from ..types import (
     Initial,
     Variable,
@@ -14,35 +13,58 @@ from ..types import (
     Independent,
     System,
 )
+from ..simulator import Simulator, Components
 from typing import Callable, Protocol, Generator, Iterable, Mapping, Sequence, TypeVar
+from ..compile import build_first_order_symbolic_ode, Backend, Array, ExprRHS, Compiled
 
 import numpy as np
-from symbolite import Symbol
+from symbolite import Real, Symbol
+from symbolite import real
 
 type VariableContainer = type[System] | SystemWrapper
 LocationVariable = TypeVar("LocationVariable")
 type Location[LocationVariable] = LocationVariable
+ArbitraryIntialVariable = TypeVar("ArbitraryIntialVariable")
+type ArbitraryInitial[ArbitraryIntialVariable] = ArbitraryIntialVariable
 type Index = int
 type InteractionType = str
+type CellType = str
 
 
 @dataclass
 class Automata:
-    cells: type[System]
-    geometry: Geometry
-    interactions: Mapping[InteractionType, Interaction]
-
+    cells: type[System] | Sequence[type[System]] | Geometry.Cells
+    geometry: type[Geometry]
+    interactions: Geometry.Interactions
+    backend: Backend = "numpy"
     def __post_init__(self):
-        self.interaction_systems = {
-            interaction: create_interaction_system(
-                interaction, Geometry.interaction_types[interaction]
-            )
-            for interaction in interactions.keys()
+        if isinstance(self.cells, type[System] | Sequence[type[System]]):
+                wrapped_cells = self.geometry.Cells(self.cells)
+        else: 
+            wrapped_cells = self.cells
+        if isinstance(self.interactions, Callable | Sequence[Callable]):
+                wrapped_interactions = self.geometry.Interactions(self.interactions)
+        else: 
+            wrapped_interactions = self.interactions
+        self.cell_systems = {cell_category: [cell for cell in wrapped_cells.__getattribute__(cell_category)] for cell_category in self.geometry.Cells.cell_categories}
+        self.interaction_systems = {interaction_category: [builder(interaction) for interaction in wrap_with_iterable(wrapped_interactions.__getattribute__(interaction_category))] for interaction_category, builder in self.geometry.Interactions.builders.items()}
+ 
+        self.symbolic_cells = {
+            cell_category: [build_first_order_symbolic_ode(cell) for cell in cells] for cell_category, cells in self.cell_systems.items()
+        }
+        self.symbolic_interactions = {
+            interaction_category: [build_first_order_symbolic_ode(interaction) for interaction in interactions]
+            for interaction_category, interactions in self.interaction_systems.items()
         }
 
-    # interact: Callable[[type, type[System] | SystemWrapper, type[System] | SystemWrapper], None]
-    # boundary: Callable[[type, type[System]], None]
-
+        self.cell_sims = {
+            cell_category: [Simulator(cell) for cell in cells]
+            for cell_category, cells in self.cell_systems.items()
+        }        
+        self.interaction_sims = {
+            interaction_category: [Simulator(interaction) for interaction in interactions]
+            for interaction_category, interactions in self.interaction_systems.items()
+        }
 
 type Interaction = Callable[
     [type, type[System] | SystemWrapper, type[System] | SystemWrapper], None
@@ -52,16 +74,119 @@ type Boundary = Callable[[type, type[System]], None]
 
 
 class Geometry(Protocol):
-    def __init__(self, shape): ...
+    cell_counts: Mapping[CellType, Sequence[int]]
+
+    def __init__(self, shape): ... 
+
     def iter_neighbors(
         self, cell: Location
     ) -> Iterable[tuple[Location, InteractionType]]: ...
 
     def get(self, location: Location) -> Index: ...
 
-    def yield_locations(self) -> Iterable[Location]: ...
+    def yield_locations(self) -> Iterable[tuple[Location, CellType, Index]]: ...
 
+    def calculate_cell_counts(self) -> Mapping[CellType, Sequence[int]]: ...
+    
+    def create_initials(self, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial]]]) -> Array: ...
+    
+    def create_parameters(self, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial]]]) -> Array: ...
+    
+    class Interactions:
+        builders: Mapping[InteractionType, Callable]
+        def __init__(self, *args) -> None: ...
+        
+    class Cells:
+        cell_categories: Sequence[CellType]     
+        def __init__(self, *args) -> None: ... 
+           
+class BaseGeometry:
+    def __init__(self, shape): 
+        self.cell_counts = self.calculate_cell_counts()
 
+    def iter_neighbors(
+        self, cell: Location
+    ) -> Iterable[tuple[Location, InteractionType]]: ...
+
+    def get(self, location: Location) -> Index: ...
+
+    def yield_locations(self) -> Iterable[tuple[Location, CellType, Index]]: ...
+
+    class Interactions:
+        builders: Mapping[InteractionType, Callable]
+        def __init__(self, *args) -> None: ...
+        
+    class Cells:
+        cell_categories: Sequence[CellType]     
+        def __init__(self, *args) -> None: ... 
+
+    def calculate_cell_counts(self) -> Mapping[CellType, Sequence[int]]:
+        counts = {cell_category: [0] for cell_category in self.Cells.cell_categories}
+        for loc, category, index in self.yield_locations():
+            try:
+                counts[category][index] +=1
+            except IndexError:
+                counts[category] += [0] * (index - len(category) + 1)
+        return counts
+    
+    # TODO: initials set from constant won't respond to changes in constants from values 
+    def create_default_initials(self, cell_category: CellType, index: Index, cell_sims: Mapping[CellType, Sequence[Simulator]], variable_index: int) -> Array:
+        y_0 = cell_sims[cell_category][index].create_problem().y[variable_index]
+        return np.full(self.cell_counts[cell_category][index], y_0)
+        
+    
+    def create_cell_initials(self, cell_category: CellType, index: Index, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial[Array]]]]) -> Array:
+        vars = symbolic_cells[cell_category][index].variables
+        intials = [
+                np.ravel(np.asarray(
+                    values[cell_category][index].get(
+                        vars[i],
+                        self.create_default_initials(cell_category=cell_category, index=index, cell_sims=cell_sims, variable_index=i),
+                    ), dtype=float)
+                )
+                for i in range(len(vars))
+        ]
+        return np.ravel(np.column_stack(intials))
+
+    def create_initials(self, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial[Array]]]]) -> Array:
+        initials = []
+        for cell_category, count_list in self.cell_counts.items():
+            for index in range(len(count_list)):
+                initials.append(self.create_cell_initials(cell_category=cell_category, index=index, symbolic_cells=symbolic_cells, cell_sims=cell_sims, values=values))
+            
+        return np.concatenate(initials)
+
+    def create_default_parameters(self, cell_category: CellType, index: Index, cell_sims: Mapping[CellType, Sequence[Simulator]], parameter_index: int) -> Array:
+        p_0 = cell_sims[cell_category][index].create_problem().p[parameter_index]
+        return np.full(self.cell_counts[cell_category][index], p_0)
+        
+    
+    def create_cell_parameters(self, cell_category: CellType, index: Index, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial[Array]]]]) -> Array:
+        params = symbolic_cells[cell_category][index].parameters
+        intials = [
+                np.ravel(np.asarray(
+                    values[cell_category][index].get(
+                        params[i],
+                        self.create_default_parameters(cell_category=cell_category, index=index, cell_sims=cell_sims, parameter_index=i),
+                    ), dtype=float)
+                )
+                for i in range(len(params))
+        ]
+        return np.ravel(np.column_stack(intials))
+
+    def create_parameters(self, symbolic_cells: Mapping[CellType, Sequence[Compiled[Variable | Derivative, dict[Variable | Derivative, ExprRHS]]]], cell_sims: Mapping[CellType, Sequence[Simulator]], values: Mapping[CellType, Sequence[Mapping[Components | real.Real, ArbitraryInitial[Array]]]]) -> Array:
+        initials = []
+        for cell_category, count_list in self.cell_counts.items():
+            for index in range(len(count_list)):
+                initials.append(self.create_cell_parameters(cell_category=cell_category, index=index, symbolic_cells=symbolic_cells, cell_sims=cell_sims, values=values))
+            
+        return np.concatenate(initials)
+    
+def wrap_with_iterable(a: object) -> Iterable: 
+    if isinstance(a, Iterable):
+        return a
+    else:
+        yield a
 class Square(Geometry):
     interaction_types = {"cell_interaction": 2, "boundary": 1}
 
@@ -91,6 +216,16 @@ class Square(Geometry):
     def get(self, location: Location) -> Index:
         strides = calculate_strides(self.shape, len(self.shape))
         return int(np.sum(strides * location))
+
+    class Cells:
+        def __init__(self):
+            self.cell_categories: Sequence[Sequence[str]]
+
+    class Interactions:
+        def __init__(self, interaction: Callable[[type[System], VariableContainer, VariableContainer], None], boundary: Callable[[type[System], VariableContainer], None] ):
+            self.interaction = interaction
+            self.boundary= boundary
+            self.builders = {"interaction": }
 
 
 def calculate_strides(shape, dim):
@@ -182,7 +317,7 @@ class CellInteraction(System, abstract=True):
                         wrappers[name][str(attribute)].__set_name__(cls, str(attribute))
                         setattr(cls, str(attribute), wrappers[name][str(attribute)])
                 elif isinstance(attribute, Parameter):
-                    if isinstance(attribute.default, Symbol):
+                    if isinstance(attribute.default, Real):
                         wrappers[name][str(attribute)] = Parameter(
                             default=attribute.default.subs(
                                 {
@@ -218,6 +353,7 @@ class CellInteraction(System, abstract=True):
         return super().__init_subclass__()
 
 
+# TODO: modify so it can take different internal and external cells, change name to build_binary_interaction
 def create_cell_interaction(
     interaction: Interaction, cell: type[System]
 ) -> type[System]:
