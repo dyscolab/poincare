2a73503080d7d111200daf2613bf2213b5d8872e
Oliverio Starosta <oliveriostarosta@gmail.com>
2025-12-30 17:49:34 -0300

    Added test for units in RateLaw and MassAction

--- src/poincare/reactions/tests/test_reactions.py
+++ src/poincare/reactions/tests/test_reactions.py
@@ -5,6 +5,9 @@ from ...simulator import Simulator
 
 from pytest import mark
 import numpy as np
+import pint
+
+ureg = pint.UnitRegistry()
 
 
 def non_instance(model):
@@ -85,4 +88,54 @@ def test_mass_action_in_simulator(f):
     sim = Simulator(Model)
     result = sim.solve(save_at=np.linspace(0, 1, 5))
     assert result.columns.tolist() == ["x"]
-    assert sim.compiled.func(0, [2], [0], [0]) == np.array([6.0])  # TODO right result?
+    assert sim.compiled.func(0, [2], [0], [0]) == np.array([6.0])
+
+
+def test_units_in_rate_law():
+    class Model(System):
+        x: Variable = initial(default=0)
+        c: Constant = assign(default=0, constant=True)
+        eq1 = RateLaw(reactants=[x], products=[], rate_law=c)
+        eq2 = RateLaw(reactants=[], products=[x], rate_law=c)
+        eq3 = RateLaw(reactants=[2 * x], products=[3 * x], rate_law=c)
+
+    class UnitModel(System):
+        x: Variable = initial(default=0 * ureg.mol)
+
+        eq1 = RateLaw(reactants=[x], products=[], rate_law=1 * ureg.mol / ureg.s)
+        eq2 = RateLaw(reactants=[], products=[x], rate_law=1 * ureg.mol / ureg.s)
+        eq3 = RateLaw(
+            reactants=[2 * x], products=[3 * x], rate_law=1 * ureg.mol / ureg.s
+        )
+
+    assert UnitModel._eq1_rate_law.default == 1 * ureg.mol / ureg.s
+    sim_1 = Simulator(Model)
+    result_1 = np.asarray(sim_1.solve(save_at=np.linspace(0, 10, 10)))
+    sim_2 = Simulator(Model)
+    result_2 = np.asarray(sim_2.solve(save_at=np.linspace(0, 10, 10)).pint.dequantify())
+    assert np.all(result_1 == result_2)
+
+
+def test_units_in_mass_action():
+    class Model(System):
+        x: Variable = initial(default=0)
+        c: Constant = assign(default=0, constant=True)
+        eq1 = MassAction(reactants=[x], products=[], rate=c)
+        eq2 = MassAction(reactants=[], products=[x], rate=c)
+        eq3 = MassAction(reactants=[2 * x], products=[3 * x], rate=c)
+
+    class UnitModel(System):
+        x: Variable = initial(default=0 * ureg.mol)
+
+        eq1 = MassAction(reactants=[x], products=[], rate=1 / ureg.s)
+        eq2 = MassAction(reactants=[], products=[x], rate=1 * ureg.mol / ureg.s)
+        eq3 = MassAction(
+            reactants=[2 * x], products=[3 * x], rate=1 / ureg.s / ureg.mol
+        )
+
+    assert UnitModel._eq2_rate_law.default == 1 * ureg.mol / ureg.s
+    sim_1 = Simulator(Model)
+    result_1 = np.asarray(sim_1.solve(save_at=np.linspace(0, 10, 10)))
+    sim_2 = Simulator(Model)
+    result_2 = np.asarray(sim_2.solve(save_at=np.linspace(0, 10, 10)).pint.dequantify())
+    assert np.all(result_1 == result_2)
