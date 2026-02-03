4afef2f3a640123832d77f61d1334a50229f3d82
Oliverio Starosta <oliveriostarosta@gmail.com>
2026-02-03 11:39:01 -0300

    Exposed reaction_initial

--- src/poincare/reactions/__init__.py
+++ src/poincare/reactions/__init__.py
@@ -1,3 +1,3 @@
-from .reactions import MassAction, RateLaw, Reactant
+from .reactions import MassAction, RateLaw, Reactant, reaction_initial
 
-__all__ = ["RateLaw", "MassAction", "Reactant"]
+__all__ = ["RateLaw", "MassAction", "Reactant", "reaction_initial"]
