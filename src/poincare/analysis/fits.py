568c57c64b6e7cb978e1ddfeded0a8e89310a137
Oliverio Starosta <oliveriostarosta@gmail.com>
2026-08-18 16:54:08 -0300

    full basic fit_result

--- src/poincare/analysis/fits.py
+++ src/poincare/analysis/fits.py
@@ -19,19 +19,19 @@ def get_default_or_inital(obj: Node) -> Initial:
 
 
 class UnitsHandler:
-    def __init__(self, fit_variables, fit_parameters, results, p0) -> None:
+    def __init__(self, fit_variables, fit_parameters, results, clean_p0) -> None:
         self.var_units = {}
         self.param_units = {}
 
         for var in fit_variables:
             res = results[var]
             if isinstance(res, pint.Quantity):
-                self.var_units[var] = res.units
+                self.var_units[var] = res.pint.units
             else:
                 self.var_units[var] = None
 
         for param in fit_parameters:
-            default = p0.get(param, get_default_or_inital(param))
+            default = clean_p0.get(param, get_default_or_inital(param))
             if isinstance(default, pint.Quantity):
                 self.param_units[param] = default.units
             else:
@@ -50,8 +50,15 @@ class UnitsHandler:
         self, obj: Initial | Sequence[Initial], var: Components | str
     ) -> Initial | Sequence[Initial]:
         if isinstance(obj, pint.Quantity):
-            return obj.to(self.var_units[var]).magnitude
+            try:
+                return obj.to(self.var_units[var]).magnitude
+            except pint.DimensionalityError as err:
+                raise pint.PintError(f"Unexpected units in {var}: {err}")
+            except AttributeError:
+                raise pint.PintError(f"units given in {param} when none were excpected")
         else:
+            if self.var_units[var] is not None:
+                raise(pint.PintError(f"No units given for variable {var}, expected dimensionality {self.var_units[var].dimensionality}"))
             return obj
 
     def quantify_parameter(
@@ -67,7 +74,12 @@ class UnitsHandler:
         self, obj: Initial | Sequence[Initial], param: Components
     ) -> Initial | Sequence[Initial]:
         if isinstance(obj, pint.Quantity):
-            return obj.to(self.param_units[param]).magnitude
+            try:
+                return obj.to(self.param_units[param]).magnitude
+            except pint.DimensionalityError as err:
+                raise pint.PintError(f"Unexpected units in {param}: {err}")
+            except AttributeError:
+                raise pint.PintError(f"units given in {param} when none were excpected")
         else:
             return obj
 
@@ -75,41 +87,41 @@ class UnitsHandler:
         if obj.pint.units is not None:
             return obj.pint.to(self.var_units[var]).pint.dequantify().to_numpy()
         else:
-            return obj
+            return obj.to_numpy()
 
-
-# "A": fix
-# "A": initial value
-# "A": (initial, min, max)
-
-# cruzado por tengo los default
-
-
+    def get_save_at(self, ds: xr.Dataset) -> Sequence | pint.Quantity:
+        index = ds[list(ds.coords)[0]]
+        if index.pint.units is not None:
+            return index.pint.dequantify().to_numpy() * index.pint.units
+        else:
+            return index.to_numpy()
+        
+    def dequantify_bounds(self, bounds: Mapping[Components, tuple[Initial, Initial]], param: Components, i: int) ->  tuple[Number]:
+        try:
+            return self.dequantify_parameter(bounds[param][i], param) 
+        except KeyError:
+            return (-1)**(i+1) * np.inf
+        
 def fit_result(
     sim: Simulator,
-    results: Mapping[Components | str, Sequence[Initial]],
-    save_at: ArrayLike,
-    fit_parameters: Iterable[Components] | None = None,
-    p0: Mapping[Components, Initial] = {},  # read only
+    results: xr.Dataset,
+    p0: Mapping[Components, Initial |tuple[Initial | None, Initial, Initial] | None] = {},  # read only
     scale: Mapping[Components | str, Number] | None = None,
     **kwargs,
 ):
     fit_variables = list(results.keys())
-    if fit_parameters is None:
-        fit_parameters = list(
-            sim.model._yield(Parameter)
-        )  # TODO: infer from p0 instead? Or in addition to this?
+    fit_parameters = list(p0.keys())
+    clean_p0, bounds = parse_p0(p0) 
     if scale is not None:
         scale = np.array([[scale.get(var, 1)] for var in fit_variables])
     else:
         scale = np.array([[1] for var in fit_variables])
-
-    units = UnitsHandler(fit_variables, fit_parameters, results, p0)
-
+    units = UnitsHandler(fit_variables, fit_parameters, results, clean_p0)
+    bounds = tuple(np.array([units.dequantify_bounds(bounds=bounds, param=param, i = i) for param in fit_parameters]) for i in range(2))
     y0 = np.array(
-        [units.dequantify_variable(results[var], var) for var in fit_variables]
+        [units.dequantify_result(results[var], var) for var in fit_variables]
     )
-
+    save_at = units.get_save_at(results)
     def f(x):
         x = [
             units.quantify_parameter(x[i], param)
@@ -129,14 +141,80 @@ def fit_result(
     x0 = np.array(
         [
             units.dequantify_parameter(
-                p0.get(param, get_default_or_inital(param)), param
+                clean_p0.get(param, get_default_or_inital(param)), param
             )
             for param in fit_parameters
         ]
     )
-    solution = least_squares(f, x0, **kwargs)
+    solution = least_squares(f, x0, bounds = bounds, **kwargs)
     formatted_solution = {
         var: units.quantify_parameter(solution.x[i], var)
         for i, var in enumerate(fit_parameters)
     }
     return formatted_solution
+
+
+def parse_p0(p0: Mapping[Components, None | Initial| Sequence[Initial]]) -> tuple[Mapping[Components, Initial], Mapping[Components, tuple[Initial, Initial]]]:    
+    clean_p0 = {}
+    bounds = {}
+    is_tuple = False
+    for param, value in p0.items():
+        try:
+            n = len(value)
+            is_tuple = True
+        except TypeError:
+            is_tuple = False
+
+        if is_tuple:
+            if n != 3:
+                raise TypeError("p0 values must be None, an initial condition or a tuple (initial condition, lower bound, upper bound)")
+            if value[0] is not None:
+                clean_p0[param] = value[0] 
+            bounds[param] = (value[1], value[2])
+        elif value is not None:
+            clean_p0[param] = value
+    return clean_p0, bounds
+
+
+def make_target(
+    results: Mapping[Components | str, Sequence | pint.Quantity],
+    save_at: Sequence | pint.Quantity,
+) -> xr.Dataset:
+    ureg = pint.get_application_registry()
+
+
+    ureg.force_ndarray_like = True
+
+    try:
+        data_vars = {}
+
+        if isinstance(save_at, pint.Quantity):
+            units_map = {"time": save_at.units}
+            dequantified_save_at = save_at.magnitude
+
+        else:
+            units_map = {}
+            dequantified_save_at = save_at
+
+        for key, value in results.items():
+            if isinstance(value, pint.Quantity):
+                units_map[key] = value.units
+                dequantified_value = value.magnitude  
+            else:
+                dequantified_value = value
+
+            data_vars[key] = xr.DataArray(
+                dequantified_value,
+                dims=["time"],
+            )
+
+        ds = xr.Dataset(data_vars=data_vars, coords={"time": dequantified_save_at})
+
+        ds = ds.pint.quantify(units_map)
+
+    finally:
+        ureg.force_ndarray_like = False
+
+    return ds
+
+
---
+++ src/poincare/analysis/tests/test_fits.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pint
+import pytest
+
+from ... import (
+    Parameter,
+    Simulator,
+    System,
+    Variable,
+    Derivative,
+    assign,
+    initial,
+)
+from ..fits import fit_result, make_target
+
+ureg = pint.get_application_registry()
+
+def test_fit_without_units():
+    class Oscillator(System):
+        x: Variable = initial(default=1)
+        vx: Derivative = x.derive(initial=0)
+
+        omega: Parameter = assign(default=1)
+
+
+        spring = vx.derive() << -omega**2 * x
+
+    sim = Simulator(Oscillator)
+    t = np.linspace(0,10,100)
+    omega_real = 2
+    target_x = np.cos(omega_real*t)
+    target_v = -omega_real* np.sin(omega_real*t)
+    target = make_target(results={"x": target_x, Oscillator.vx: target_v}, save_at= t)
+
+    with pytest.raises(pint.PintError):
+        fit = fit_result(sim=sim, results=target, p0 = {Oscillator.omega: omega_real*1.1, Oscillator.x: (None, -np.inf*ureg.m, 10)}, )
+
+    fit = fit_result(sim=sim, results=target, p0 = {Oscillator.omega: omega_real*1.1, Oscillator.x: (None, -np.inf, 10)}, )
+
+    rtol = 0.025
+    assert np.isclose(fit[Oscillator.x], Oscillator.x.initial, rtol=rtol)
+    assert np.isclose(fit[Oscillator.omega],omega_real, rtol=rtol) 
+
+def test_fit_with_units():
+    class UnitsOscillator(System):
+        x: Variable = initial(default=1* ureg.m)
+        vx: Derivative = x.derive(initial=0* ureg.m/ureg.s)
+
+        omega: Parameter = assign(default=1 *1/ureg.s)
+
+
+        spring = vx.derive() << -omega**2 * x
+
+    usim = Simulator(UnitsOscillator)
+    u_t = np.linspace(0,10,100) * ureg.s
+    u_omega_real = 2 * 1/ureg.s
+    u_target_x = np.cos(u_omega_real*u_t) * ureg.m
+    u_target_v = -u_omega_real* np.sin(u_omega_real*u_t)
+    u_target = make_target(results={"x": u_target_x, UnitsOscillator.vx: u_target_v}, save_at=u_t)
+    u_fit = fit_result(sim=usim, results=u_target, p0 = {UnitsOscillator.omega: u_omega_real*1.1, UnitsOscillator.x: (None, -np.inf*ureg.m, 1000*ureg.cm)})
+
+    rtol = 0.025
+    assert np.isclose(u_fit[UnitsOscillator.x].to(ureg.m).magnitude, UnitsOscillator.x.initial.magnitude, rtol=rtol)
+    assert np.isclose(u_fit[UnitsOscillator.omega].to(1/ureg.s).magnitude, u_omega_real.to(1/ureg.s).magnitude, rtol=rtol) 
+    
