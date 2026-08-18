3f373dd3449f39ffa9153953d868d23b9672b9b9
Oliverio Starosta <oliveriostarosta@gmail.com>
2026-08-18 17:11:47 -0300

    added get_from  and to_values helpers

---
+++ src/poincare/helpers.py
@@ -0,0 +1,16 @@
+from collections.abc import Mapping
+
+from ._node import Node
+from .types import System, Initial
+
+def get_from(param: str, model: type[System]) -> Node:
+    path = param.split(".")
+    current = model
+    for level in path:
+        current = getattr(current, level)
+    return current
+
+def to_values(params: Mapping[str, Initial], model: type[System]) -> Mapping[Node, Initial]:
+    return {get_from(param=param, model=model): value for param, value in params.items()}
+
+__all__ = ["get_from", "to_values"]
---
+++ src/poincare/tests/test_helpers.py
@@ -0,0 +1,32 @@
+
+from ..types import Parameter, System, Variable, Derivative, initial, assign
+from ..helpers import get_from, to_values
+
+import pytest
+
+class Oscillator(System):
+    x: Variable = initial(default=1)
+    vx: Derivative = x.derive(initial=0)
+
+    omega: Parameter = assign(default=1)
+    k: Parameter = assign(default=2)
+
+
+    spring = vx.derive() << -omega**2 * x
+class Parent(System):
+    osc = Oscillator()
+class ParentParent(System):
+    subparent = Parent()
+
+def test_get_from():
+    x_str = str(ParentParent.subparent.osc.x)
+    assert x_str == "subparent.osc.x"
+    assert get_from(x_str, ParentParent) == ParentParent.subparent.osc.x 
+    with pytest.raises(AttributeError): 
+        get_from("noexistent", ParentParent)
+
+def test_to_values():
+    param_dict = to_values({"subparent.osc.x": 1}, ParentParent)
+    assert param_dict == {ParentParent.subparent.osc.x : 1}
+    with pytest.raises(AttributeError): 
+        to_values({"noexistent": 1}, ParentParent)
