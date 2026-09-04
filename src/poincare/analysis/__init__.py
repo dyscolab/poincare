921589ed278811a3718c46fc6cedd183b01f5e38
Oliverio Starosta <oliveriostarosta@gmail.com>
2026-09-04 12:30:01 -0300

    Added __init__ to analysis, exposes all analysis functions

---
+++ src/poincare/analysis/__init__.py
@@ -0,0 +1,7 @@
+from .oscillations import Oscillations
+from .steady_state import SteadyState
+from .fits import fit_result
+from .sweeper import Sweeper
+
+
+all = ["Oscillations", "SteadyState", "fit_result", "Sweeper"]
