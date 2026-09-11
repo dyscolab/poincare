6ffef910454351d6f315d1f38555625477f47371
Oliverio Starosta <oliveriostarosta@gmail.com>
2026-09-11 17:54:42 -0300

    exported make_target helper in poincare.analysis __init__

--- src/poincare/analysis/__init__.py
+++ src/poincare/analysis/__init__.py
@@ -1,6 +1,6 @@
-from .fits import fit_result
+from .fits import fit_result, make_target
 from .oscillations import Oscillations
 from .steady_state import SteadyState
 from .sweeper import Sweeper
 
-__all__ = ["Oscillations", "SteadyState", "Sweeper", "fit_result"]
+__all__ = ["Oscillations", "SteadyState", "Sweeper", "fit_result", "make_target"]
